7587854d76365fd959c88f7e913fc571f20c6d53
ecasellas <enric.casellas@gmail.com>
2024-06-14 17:21:22 +0200

    Add model run data query to parquet.py

--- postproc/io/parquet.py
+++ postproc/io/parquet.py
@@ -3,17 +3,17 @@ import duckdb
 
 def get_model_lt_data(parquet_file, lead_time, start_date, end_date):
     model_data = duckdb.query(
-                        "SELECT * FROM '"
-                        + parquet_file
-                        + "' WHERE LEAD_TIME = "
-                        + str(lead_time)
-                        + " AND RUN_DATETIME >= '"
-                        + start_date
-                        + "' AND RUN_DATETIME <= '"
-                        + end_date
-                        + "'"
-                    ).df()
-    
+        "SELECT * FROM '"
+        + parquet_file
+        + "' WHERE LEAD_TIME = "
+        + str(lead_time)
+        + " AND RUN_DATETIME >= '"
+        + start_date
+        + "' AND RUN_DATETIME <= '"
+        + end_date
+        + "'"
+    ).df()
+
     model_data.dropna(inplace=True)
 
     if len(model_data) == 0:
@@ -22,20 +22,32 @@ def get_model_lt_data(parquet_file, lead_time, start_date, end_date):
     return model_data
 
 
+def get_model_run(parquet_file, run_datetime):
+    model_data = duckdb.query(
+        "SELECT * FROM '"
+        + parquet_file
+        + "*.parquet' WHERE run_datetime = '"
+        + run_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        + "';"
+    ).df()
+
+    return model_data
+
+
 def get_station_var_data(parquet_file, variable, start_date, end_date):
 
     station_data = duckdb.query(
-                    "SELECT * FROM '"
-                    + parquet_file
-                    + "' WHERE VARIABLE = '"
-                    + variable
-                    + "' AND DATETIME >= '"
-                    + start_date
-                    + "' AND DATETIME <= '"
-                    + end_date
-                    + "'"
-                ).df()
-    
+        "SELECT * FROM '"
+        + parquet_file
+        + "' WHERE VARIABLE = '"
+        + variable
+        + "' AND DATETIME >= '"
+        + start_date
+        + "' AND DATETIME <= '"
+        + end_date
+        + "'"
+    ).df()
+
     station_data.dropna(inplace=True)
 
     if len(station_data) == 0:
